27dae888296747122f90da03f8cb5715aa7b2fec
unknown <axeljzx>
2022-11-05 02:06:17 -0500

    cambio titulo

--- src/components/Pacientes/Header.jsx
+++ src/components/Pacientes/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
     return ( 
-        <h1 className="font-extrabold text-5xl text-center mt-8 md:w-2/3 mx-auto">Seguimiento Pacientes <span className="text-indigo-700">Veterinaria</span> </h1>
+        <h1 className="font-extrabold text-5xl text-center mt-8 md:w-2/3 mx-auto">Seguimiento Pacientes Prueba<span className="text-indigo-700">Veterinaria</span> </h1>
      );
 }
  
